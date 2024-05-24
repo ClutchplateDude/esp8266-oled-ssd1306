ee80c90734f6525cd6e54350341d9f2b418857c5
Lutz <lutz@stmuc.com>
2024-05-24 00:56:20 -0700

    Renamed ctor arguments

--- src/SSD1306Wire.h
+++ src/SSD1306Wire.h
@@ -33,7 +33,6 @@
 
 #include "OLEDDisplay.h"
 #include <Wire.h>
-#include <algorithm>
 
 #if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_STM32)
 #define _min	min
@@ -53,7 +52,7 @@ class SSD1306Wire : public OLEDDisplay {
       int                 _scl;
       bool                _doI2cAutoInit = false;
       TwoWire*            _wire = NULL;
-      int                 _frequency;
+      long                _frequency;
 
   public:
 
@@ -72,18 +71,18 @@ class SSD1306Wire : public OLEDDisplay {
      * @param _i2cBus on ESP32 with 2 I2C HW buses, I2C_ONE for 1st Bus, I2C_TWO fot 2nd bus, default I2C_ONE
      * @param _frequency for Frequency by default Let's use ~700khz if ESP8266 is in 160Mhz mode, this will be limited to ~400khz if the ESP8266 in 80Mhz mode
      */
-    SSD1306Wire(uint8_t _address, int _sda = -1, int _scl = -1, OLEDDISPLAY_GEOMETRY g = GEOMETRY_128_64, HW_I2C _i2cBus = I2C_ONE, int _frequency = 700000) {
+    SSD1306Wire(uint8_t address, int sda = -1, int scl = -1, OLEDDISPLAY_GEOMETRY g = GEOMETRY_128_64, HW_I2C i2cBus = I2C_ONE, long  frequency = 700000) {
       setGeometry(g);
 
-      this->_address = _address;
-      this->_sda = _sda;
-      this->_scl = _scl;
+      this->_address = address;
+      this->_sda = sda;
+      this->_scl = scl;
 #if !defined(ARDUINO_ARCH_ESP32)
       this->_wire = &Wire;
 #else
-      this->_wire = (_i2cBus==I2C_ONE) ? &Wire : &Wire1;
+      this->_wire = (i2cBus == I2C_ONE) ? &Wire : &Wire1;
 #endif
-      this->_frequency = _frequency;
+      this->_frequency = frequency;
     }
 
     bool connect() {
@@ -118,10 +117,10 @@ class SSD1306Wire : public OLEDDisplay {
           for (x = 0; x < this->width(); x++) {
            uint16_t pos = x + y * this->width();
            if (buffer[pos] != buffer_back[pos]) {
-             minBoundY = std::min(minBoundY, y);
-             maxBoundY = std::max(maxBoundY, y);
-             minBoundX = std::min(minBoundX, x);
-             maxBoundX = std::max(maxBoundX, x);
+             minBoundY = min(minBoundY, y);
+             maxBoundY = max(maxBoundY, y);
+             minBoundX = min(minBoundX, x);
+             maxBoundX = max(maxBoundX, x);
            }
            buffer_back[pos] = buffer[pos];
          }
